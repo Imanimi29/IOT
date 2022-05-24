c6a9643c8b470ed1126620076dc4605e0c275dd0
Imanimi29 <69517258+Imanimi29@users.noreply.github.com>
2022-05-24 05:08:21 +0500

    Add files via upload

---
+++ main/java/com/example/weather/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private int temperature=60;
+    private int rain=60;
+    private int humidity=60;
+
+    ProgressBar progressBar,progressBar_Rain,progressBar_Humidity;
+    Button btn1;
+    TextView textView,textViewProgress,textView_RainValue,textView_humidityValue,textView_todayDate,title_rain,title_humidity;
+    ConstraintLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar=findViewById(R.id.progress_bar);
+        progressBar_Rain=findViewById(R.id.progress_bar2_rain);
+        progressBar_Humidity=findViewById(R.id.progress_bar1_humidity);
+        textView_RainValue=findViewById(R.id.text_view_progress2_rain);
+        textView_humidityValue=findViewById(R.id.text_view_progress1_humidity);
+        textView_todayDate=findViewById(R.id.tv_today_date);
+        layout=(ConstraintLayout)findViewById(R.id.parent_layout);
+        textView=findViewById(R.id.text_view_progress);
+
+        title_rain=findViewById(R.id.text_view_rain);
+        title_humidity=findViewById(R.id.text_view_humidity);
+        Date date = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+        String formattedDate = df.format(date);
+        textView_todayDate.setText(formattedDate);
+        title_rain.setText("Rain");
+        title_humidity.setText("Humidity");
+        btn1=findViewById(R.id.btn_refresh);
+
+
+        updateProgressBar();
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateProgressBar();
+            }
+        });
+
+
+
+        updateProgressBar();
+    }
+    private void updateProgressBar() {
+        progressBar.setProgress(temperature);
+        progressBar_Rain.setProgress(rain);
+        progressBar_Humidity.setProgress(humidity);
+        textView_humidityValue.setText(rain+"%");
+        textView_RainValue.setText(humidity+"%");
+        char degreesymbol = '\u00B0';
+        textView.setText(temperature+" F");
+        if(temperature>50)
+        {
+            layout.setBackgroundResource(R.drawable.sun);
+        }
+        else
+        {
+            layout.setBackgroundResource(R.drawable.rain);
+        }
+    }
+}
